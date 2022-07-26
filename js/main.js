3a434920da875e4a3592f353c7343d858c13a07b
SmOke11345 <103870835+SmOke11345@users.noreply.github.com>
2022-07-26 12:23:01 +0600

    modified:   css/style.css modified:   index.html modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -34,20 +34,34 @@ $('.owl-carousel').owlCarousel({
 })
 
 
-const textInCards = document.querySelector('.text-on-cards'),
-      arrowProd = document.querySelector('.arrow-products'),
-      cards = document.querySelector('.card');
-
-
-arrowProd.addEventListener('click', () => {
-    textInCards.classList.toggle('open');
-    cards.classList.toggle('open');
-    arrowProd.classList.toggle('open');
-})
-    
- 
-
-
-
+// Аккордеон 
+class ItcAccordion {
+    constructor(target, config) {
+      this._el = typeof target === 'string' ? document.querySelector(target) : target;
+      const defaultConfig = {
+        alwaysOpen: true
+      };
+      this._config = Object.assign(defaultConfig, config);
+      this.addEventListener();
+    }
+    addEventListener() {
+      this._el.addEventListener('click', (e) => {
+        const elHeader = e.target.closest('.accordion__header');
+        if (!elHeader) {
+          return;
+        }
+        if (!this._config.alwaysOpen) {
+          const elOpenItem = this._el.querySelector('.accordion__item_show');
+          if (elOpenItem) {
+            elOpenItem !== elHeader.parentElement ? elOpenItem.classList.toggle('accordion__item_show') : null;
+          }
+        }
+        elHeader.parentElement.classList.toggle('accordion__item_show');
+      });
+    }
+  }
+  new ItcAccordion('#accordion-1', {
+    alwaysOpen: false
+  });
 
 
